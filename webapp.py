7341158a0236178769ef6f8cf34688a287d93e65
Niraj2303 <117979509+Niraj2303@users.noreply.github.com>
2024-05-18 15:56:09 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -131,14 +131,14 @@ if choice=="Login":
                         sex1=0
                     else:
                         sex1=1
-                    Chestpaintype=st.selectbox("chest pain",["low","mid","High","very High"])
+                    Chestpaintype=st.selectbox("chest pain",["low","mid","High","VHigh"])
                     if Chestpaintype=="low":
                         Chestpaintype1=1
                     elif Chestpaintype=="mid":
                         Chestpaintype1=2
                     elif Chestpaintype=="High":
                         Chestpaintype1=3
-                    elif Chestpaintype=="very high":
+                    elif Chestpaintype=="Vhigh":
                         Chestpaintype1=4
                     BP1=int(st.slider('BP',94,200))
                     cholesterol1=float(st.slider('cholesterol',126.0,564.0))
